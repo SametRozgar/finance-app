6a52d6c1a71c69041bb457e2883c82a47c5a3826
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-02-07 00:22:48 +0300

    auth layout tamamlandı

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,6 +20,7 @@ import {
 import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
+import { Loader, Loader2 } from "lucide-react";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -27,19 +28,26 @@ const formSchema = z.object({
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+  const [isloading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+
+
+  const formSchema = authFormSchema(type);
+
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      password: ""
+      password: "",
     },
   });
 
-  
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
-  
+  const onSubmit= async (values: z.infer<typeof formSchema>)=> {
+    setIsLoading(true);
     console.log(values);
+    setIsLoading(false);
   }
 
   return (
@@ -69,22 +77,111 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-             
+              {type === "sign-up" && (
+                
+                
+                <>
 
 
+                <div className="flex gap-4">
+                <CustomInput
+                    control={form.control}
+                    name="firstName"
+                    label="First Name"
+                    placeholder="Enter your First Name"
+                  />
 
-            <CustomInput 
-            control={form.control} name="email" label="Email" placeholder="Enter your email"
-            />
+                  <CustomInput
+                    control={form.control}
+                    name="lastName"
+                    label="Last Name"
+                    placeholder="Enter your Last Name"
+                  />
 
-<CustomInput 
-            control={form.control} name="password" label="Password" placeholder="Enter your password"
-            />
+                </div>
+                 
+                  <CustomInput
+                    control={form.control}
+                    name="adress1"
+                    label="Adress"
+                    placeholder="Enter your Specific Adress"
+                  />
 
+                  <div className="flex gap-4">
+                  <CustomInput
+                    control={form.control}
+                    name="state"
+                    label="State"
+                    placeholder="Example: NY"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="postalCode"
+                    label="Postal Code"
+                    placeholder="Example: 11101"
+                  />
+                  </div>
+                   <div className="flex gap-4">
+                   <CustomInput
+                    control={form.control}
+                    name="dateOfBirth"
+                    label="Date Of Birth"
+                    placeholder="YYY/MM/DD"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="ssn"
+                    label="SSN"
+                    placeholder="Example: 1234"
+                  />
+                   </div>
+                 
+                  
+                </>
+              )}
 
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+              />
 
-              
-              <Button type="submit">Submit</Button>
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
+
+              <div className="flex flex-col gap-4">
+                <Button className="form-btn" disabled={isloading} type="submit">
+                  {isloading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading ...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
+
+              <footer className="flex justify-center gap-1">
+                <p className="text-14 font-normal text-gray-600">
+                  {type === "sign-in"
+                    ? "Don't have an account?"
+                    : "Already have an account?"}
+                </p>
+                <Link
+                  className="form-link"
+                  href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+                >
+                  {type === "sign-in" ? "Sign Up" : "Sign In"}
+                </Link>
+              </footer>
             </form>
           </Form>
         </>
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
 import { Input } from './ui/input'
-import {Control} from "react-hook-form"
+import {Control, FieldPath} from "react-hook-form"
 import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
+
+
+const formSchema=authFormSchema("sign-up")
+
 interface CustomInput
 {
-    control:Control<z.infer<typeof authFormSchema>>,
-    name:"email" | "password",
+    control:Control<z.infer<typeof formSchema>>,
+    name:FieldPath<z.infer<typeof formSchema>>,
     label:string,
     placeholder:string
 }
 
 const CustomInput = ({control,name,label,placeholder}:CustomInput) => {
+
   return (
     <FormField
     control={control}
@@ -27,7 +32,8 @@ const CustomInput = ({control,name,label,placeholder}:CustomInput) => {
         <Input
         placeholder={placeholder}
         className="input-class" 
-        type="password"/>
+        type={name==="password"? "password":"text"}
+        {...field }/>
         </FormControl>
         <FormMessage className="form-message mt-2"/>
       </div>
